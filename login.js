2f805039d9c45b3e6db59f5bb81583ae074e0dd5
EdogawaConan2023 <developervn01072023@gmail.com>
2023-08-15 00:26:11 +0700

    ok

---
+++ login.js
@@ -0,0 +1,19 @@
+function loGin() {
+  let tendangnhap = document.getElementById("tendangnhap").value;
+  let password = document.getElementById("password").value;
+
+  console.log(tendangnhap);
+  console.log(password);
+
+  if (tendangnhap === "" || password === "") {
+    console.log("Không được để trống tên đăng nhập hoặc mật khẩu");
+  } else if (tendangnhap === "quynh" && password === "123") {
+    console.log("Welcome Quynh");
+    document.getElementById("show_html").innerHTML = `Welcome ${tendangnhap}`;
+  } else if (tendangnhap === "an" && password === "123") {
+    console.log("Welcome An");
+    document.getElementById("show_html").innerHTML = `Welcome ${tendangnhap}`;
+  } else {
+    console.log("Bạn đã đăng nhập sai");
+  }
+}
