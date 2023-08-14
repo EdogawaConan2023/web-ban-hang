460e2197e252f56091d1509551769a05d7c5cde4
EdogawaConan2023 <developervn01072023@gmail.com>
2023-08-15 02:20:32 +0700

    Quynh update new

--- login.js
+++ login.js
@@ -10,9 +10,21 @@ function loGin() {
   } else if (tendangnhap === "quynh" && password === "123") {
     console.log("Welcome Quynh");
     document.getElementById("show_html").innerHTML = `Welcome ${tendangnhap}`;
+
+    var userData = {
+      username: tendangnhap,
+      isLoggedIn: true,
+    };
+    localStorage.setItem("dataLogin", JSON.stringify(userData));
   } else if (tendangnhap === "an" && password === "123") {
     console.log("Welcome An");
     document.getElementById("show_html").innerHTML = `Welcome ${tendangnhap}`;
+
+    var userData = {
+      username: tendangnhap,
+      isLoggedIn: true,
+    };
+    localStorage.setItem("dataLogin", JSON.stringify(userData));
   } else {
     console.log("Bạn đã đăng nhập sai");
   }
